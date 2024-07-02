d4c73026892a0166950091f84639be174b855035
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-02 17:49:28 +0200

    First working version

---
+++ src/main/java/spring/Parrot.java
@@ -0,0 +1,12 @@
+package spring;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Parrot {
+    private String name;
+}
