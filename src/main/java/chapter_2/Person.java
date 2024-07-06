f04573cf012920888739cf369b3196e85fbd43ec
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-06 21:14:08 +0200

    Add Autowired to constructor

--- src/main/java/chapter_2/Person.java
+++ src/main/java/chapter_2/Person.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
 @ToString
 @Component
 public class Person {
     private String name;
+    private final Parrot2 parrot2;
+
     @Autowired
-    private Parrot2 parrot2;
+    public Person(Parrot2 parrot2) {
+        this.parrot2 = parrot2;
+    }
 }
