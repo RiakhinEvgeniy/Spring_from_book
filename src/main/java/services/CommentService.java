f237feb2c911996dde074657f37444b8a0d8080e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-15 18:56:08 +0200

    Added Logger from JDK

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Service;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
+import java.util.logging.Logger;
+
 @Getter
 @Service
 public class CommentService {
+    private final Logger log = Logger.getLogger(CommentService.class.getName());
     private final CommentNotificationProxy commentNotificationProxy;
     private final CommentRepository commentRepository;
 
@@ -21,6 +24,7 @@ public class CommentService {
     }
 
     public void publishComment(Comment comment) {
+        log.info("Publishing comment-> " + comment.getText());
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
